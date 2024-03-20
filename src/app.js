a895cb2f29c17fe4c9e2b93d849a5c67c0d76d89
Crautor <vitor.dallanol@gmail.com>
2024-03-20 11:35:16 -0300

    feat: add some introduction message from api

--- src/app.js
+++ src/app.js
@@ -16,5 +16,18 @@ connection();
 const routes = require('./routes/router');
 
 const port = process.env.PORT ? Number(process.env.PORT) : null;
+const URL = process.env.URL || 'http://localhost:3030';
 app.use('/api', routes);
-app.listen(port, () => console.log('Server is running on port', port));
+app.listen(
+  port,
+  () => console.log('Server is running on port', port),
+  console.log('Server is runnin at ', URL)
+);
+
+app.get('/', (req, res) => {
+  res
+    .status(200)
+    .send(
+      'Welcome to the API of the project "AnatonellyServer" of the company "AnatonellyTech" enjoy!. If you want to know more about the project, contact: (45) 99153-6651'
+    );
+});
